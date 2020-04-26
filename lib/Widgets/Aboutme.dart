d6259e747f94e33772ee9bc2462af6b92110b0a5
Uzair Leo <uzair.jan336@gmail.com>
2020-04-26 15:53:09 +0500

    updated aboutme

--- lib/Widgets/Aboutme.dart
+++ lib/Widgets/Aboutme.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+
+  var password="imaandaro123";
+
+  
+
 info(BuildContext context) {
     showDialog(
         context: context,
@@ -93,4 +99,65 @@ info(BuildContext context) {
     }
 
 
+  }
+  showPassword(context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return  Dialog(
+            elevation: 0.0,
+            backgroundColor: Colors.transparent,
+            child: Container(
+              height: 300.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Wifi Key",
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Satisfy'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Text("Alert ! please. This is the key "
+                "which you can used in all of your devices "
+                "for connecting with SSID : iAmUzairLeO. Make "
+                "Sure you dont share this with anyOne in case "
+                "you share this with anyone then supp-ose he will "
+                "exceed the limit and in return your IP adress "
+                "will be blocked by the protocol Automatically",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom:8.0,top: 3.0),
+                    child: Text("Key",
+                    style: TextStyle(
+                      fontSize:18.0
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 18.0),
+                    child: Text(
+                      password,
+                      style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+          );
+        });
   }
